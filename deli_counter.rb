5974ed75e297f266b88ffc94f799a4d1c53da864
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 23:52:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    "The line is empty."
+  else 
+    "The current line of people is: #{katz_deli[i]}"
+  end
+  katz_deli
+end
