fb0c0eecb869fb45d626c1b131bd5ce3fa0b6cc4
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 00:00:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,10 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    
+end
